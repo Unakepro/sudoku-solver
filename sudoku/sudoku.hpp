a5363def2e3c0eec5d86b73dba2b209340896384
Andrew <chebanov.andrew@gmail.com>
2024-09-15 15:21:29 +0200

    inital file structure

---
+++ sudoku/sudoku.hpp
@@ -0,0 +1,24 @@
+#ifndef SUDOKU
+#define SUDOKU
+
+#include <iostream>
+
+template <size_t n>
+class Sudoku {
+    std::vector<std::vector<size_t>> grid;
+
+public:
+    Sudoku(); 
+
+    // void set_position(size_t i, size_t j, size_t value) {
+
+    // }
+
+    // void get_value(size_t i, size_t j) {
+
+    // }
+
+};
+
+
+#endif
